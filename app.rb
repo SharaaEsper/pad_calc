1e2d7c2ae02b8e6f44b8572d2792032385ccd0f4
Ryuujinx <admin@failverse.com>
2015-11-05 21:24:38 +0000

    Added stuff for Unicorn

--- app.rb
+++ app.rb
@@ -3,25 +3,20 @@
 require 'rubygems'
 require 'sinatra'
 Dir[File.dirname(__FILE__) + '/include/*.rb'].each {|file| require file }
+class Calc_app < Sinatra::Base
 
-set :environment, :production
+	set :environment, :production
+	get '/' do
+		redirect '/lookup'
+	end
+
+	get '/lookup' do
+		erb :lookup
+	end
 
 
-get '/' do
-	redirect '/lookup'
+	get '/lookup/:username' do
+		@end_result,@total_mp = Pad_calc.lookup( params[:username] )
+		erb :lookupreturn
+	end
 end
-
-get '/lookup' do
-	erb :lookup
-end
-
-
-get '/lookup/:username' do
-	@end_result,@total_mp = Pad_calc.lookup( params[:username] )
-	erb :lookupreturn
-end
-
-get '/hi' do
-	"Hello World!"
-end
-
