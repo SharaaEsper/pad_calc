e2eb2e193ce20add7a192e43de7a056bb50ef7f8
James Dewey <admin@failverse.com>
2016-07-21 14:41:20 -0500

    Update cron.rb
    Fixed formatting

--- cron.rb
+++ cron.rb
@@ -14,31 +14,31 @@ agent = Mechanize.new
 mapping = Hash.new
 
 
-		mid = 0
-    buffer = open("https://www.padherder.com/api/monsters").read
-    monster_json = JSON.parse(buffer)
-		monster_json.each do |foo|
-			#Check if it has a pdx_id we should be mapping to instead
-			if foo.has_key? "pdx_id"
-				mid = foo["pdx_id"]
-			else
-				mid = foo["id"]
-			end
-      page = agent.get("http://puzzledragonx.com/en/monster.asp?n=#{mid}")
+mid = 0
+buffer = open("https://www.padherder.com/api/monsters").read
+monster_json = JSON.parse(buffer)
+monster_json.each do |foo|
+#Check if it has a pdx_id we should be mapping to instead
+	if foo.has_key? "pdx_id"
+		mid = foo["pdx_id"]
+	else
+		mid = foo["id"]
+	end
+	page = agent.get("http://puzzledragonx.com/en/monster.asp?n=#{mid}")
         arr =  page.search('//table[@class = "tableprofile"]/tr/td[@class = "data"]')
         mp = arr[-1].to_s.scan(/>(.*)</)[0][0].to_i
         if mp == 0
                 mp = arr[-2].to_s.scan(/>(.*)</)[0][0].to_i
                 if mp == 0
-                mp = arr[-3].to_s.scan(/>(.*)</)[0][0].to_i
+        	        mp = arr[-3].to_s.scan(/>(.*)</)[0][0].to_i
                 end
         end
-			puts "Processing: #{mid} \r"	
-			mapping[mid.to_i] = mp.to_i
+		puts "Processing: #{mid} \r"	
+		mapping[mid.to_i] = mp.to_i
 end
 
 f =File.open("include/mapping.json.tmp", "w")
-	f.write(mapping.to_json)
+f.write(mapping.to_json)
 f.close
 
 FileUtils.rm('include/mapping.json.bk', :force => true)
